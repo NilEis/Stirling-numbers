b4d086d424f4ba0258adba69ff02c00fcb806866
NilEis <52820950+NilEis@users.noreply.github.com>
2022-01-18 18:26:37 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,11 +2,19 @@
 #include <gmp.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <string.h>
 #include "include/terminal.h"
 
-void sterling(uint32_t n, uint32_t k, mpz_t res);
+void stirling_recursion(uint32_t n, uint32_t k, mpz_t res);
+void stirling_iterative(uint32_t n, uint32_t k, mpz_t res);
 int main(int argc, char const *argv[])
 {
+
+	/*FILE *f = fopen("numHex.txt", "w");
+	mpz_t x;
+	mpz_init_set_ui(x, 0);
+	mpz_fac_ui(x, 20000000);
+	gmp_fprintf(f, "%Zx\n", x);*/
 	uint32_t n = 0;
 	uint32_t k = 0;
 	mpz_t sterling_res;
@@ -17,16 +25,69 @@ int main(int argc, char const *argv[])
 	terminal_clear();
 	printf("k: ");
 	scanf("%" SCNu32, &k);
-	sterling(n, k, sterling_res);
-	gmp_printf("Sterling(n=%" PRIu32 ", k=%" PRIu32 ")=%Zd\n", n, k, sterling_res);
+	printf("n=%" PRIu32 ", k=%" PRIu32 "\n", n, k);
+	stirling_iterative(n, k, sterling_res);
+	gmp_printf("Sterling(n=%" PRIu32 ", k=%" PRIu32 ")=%Zd\n\n\n%Zx\n", n, k, sterling_res, sterling_res);
 	mpz_clear(sterling_res);
-	while(terminal_kbhit()!=1)
+	while (terminal_kbhit() != 1)
 	{
 		asm("nop\n");
 	}
 }
 
-void sterling(uint32_t n, uint32_t k, mpz_t res)
+void stirling_iterative(uint32_t n, uint32_t k, mpz_t res)
+{
+	mpz_t loop_res_div;
+	mpz_t loop_res;
+	mpz_t tmp_loop_res;
+	mpz_t bin;
+	mpz_t neg;
+	mpz_t tmp_neg;
+	mpz_t po;
+	mpz_t tmp_po;
+	mpz_init(loop_res_div);
+	mpz_init(loop_res);
+	mpz_init(tmp_loop_res);
+	mpz_fac_ui(loop_res_div, k);
+	mpz_init(bin);
+	mpz_init(neg);
+	mpz_init(tmp_neg);
+	mpz_init(po);
+	mpz_init(tmp_po);
+	uint32_t p = k / (terminal_get_width() - 1);
+	for (unsigned long i = 0; i <= k; i++)
+	{
+		mpz_set_si(loop_res, 0);
+		mpz_set_si(tmp_loop_res, 0);
+		mpz_set_si(bin, 0);
+		mpz_set_si(neg, 0);
+		mpz_set_si(po, 0);
+		mpz_set_si(tmp_po, 0);
+		mpz_set_si(tmp_neg, -1);
+		mpz_pow_ui(neg, tmp_neg, k - i);
+		mpz_bin_uiui(bin, k, i);
+		mpz_set_si(tmp_po, i);
+		mpz_pow_ui(po, tmp_po, n);
+		mpz_mul(tmp_loop_res, neg, bin);
+		mpz_mul(loop_res, tmp_loop_res, po);
+		if (!(i % p))
+		{
+			printf("#");
+		}
+	}
+	mpz_fdiv_q(res, loop_res, loop_res_div);
+
+	mpz_clear(loop_res_div);
+	mpz_clear(loop_res);
+	mpz_clear(tmp_loop_res);
+	mpz_clear(bin);
+	mpz_clear(neg);
+	mpz_clear(po);
+	mpz_clear(tmp_po);
+	mpz_clear(tmp_neg);
+}
+
+void stirling_recursion(uint32_t n, uint32_t k, mpz_t res)
 {
 	if (n == k)
 	{
@@ -60,7 +121,7 @@ void sterling(uint32_t n, uint32_t k, mpz_t res)
 		mpz_sub(res, res, tmp);
 		mpz_clear(tmp);
 		mpz_add_ui(res, res, 1);
-		mpz_div_ui(res, res, 3);
+		mpz_fdiv_q_ui(res, res, 3);
 
 		return;
 	}
@@ -82,7 +143,7 @@ void sterling(uint32_t n, uint32_t k, mpz_t res)
 		mpz_sub_ui(res, res, n - 2);
 		mpz_sub_ui(res, res, n - 1);
 		mpz_sub_ui(res, res, n);
-		mpz_div_ui(res, res, 24);
+		mpz_fdiv_q_ui(res, res, 24);
 		return;
 	}
 	if ((n - 1) == k)
@@ -90,10 +151,10 @@ void sterling(uint32_t n, uint32_t k, mpz_t res)
 		mpz_bin_uiui(res, n, 2);
 		return;
 	}
-	sterling(n - 1, k - 1, res);
+	stirling_recursion(n - 1, k - 1, res);
 	mpz_t tmp;
 	mpz_init_set(tmp, res);
-	sterling(n - 1, k, tmp);
+	stirling_recursion(n - 1, k, tmp);
 	mpz_mul_ui(tmp, tmp, k);
 	mpz_add(res, res, tmp);
 	mpz_clear(tmp);
